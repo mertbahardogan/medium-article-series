433f0d07e7e812c5861b23210b211b9634585bf8
mertbahardogan <mert.bahardogan@icterra.com>
2021-12-28 01:24:07 +0300

    First Api has created.

--- product/src/main/java/com/microservices/project/ProductApplication.java
+++ product/src/main/java/com/microservices/project/ProductApplication.java
@@ -4,8 +4,8 @@ import com.microservices.project.product.dataAccess.ProductDAO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.*;
@@ -19,7 +19,7 @@ import java.util.List;
 
 @SpringBootApplication
 @EnableMongoRepositories(basePackageClasses = ProductDAO.class)
-//@Configuration
+@EnableWebMvc
 public class ProductApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductApplication.class, args);
